1783ac0fe4018b55bfd19d8f402405c13d666550
Calle Svensson <calle.svensson@zeta-two.com>
2024-12-18 03:22:02 +0100

    Test round-trip from PYthon

--- genieutils-rspy/test/test_basic.py
+++ genieutils-rspy/test/test_basic.py
@@ -5,12 +5,9 @@ import genieutils_rspy
 with open('test/empires2_x2_p1.dat', 'rb') as fin:
     compressed = fin.read()
 
-x = genieutils_rspy.DatFile.parse_compressed(compressed)
-print(f'Version: {x.version}')
-print(dir(x))
+decompressed = genieutils_rspy.DatFile.decompress(compressed)
+datfile = genieutils_rspy.DatFile.parse(decompressed)
+print(f'Version: {datfile.version}')
+serialized = datfile.serialize()
 
-recompressed = x.pack()
-
-print(type(compressed))
-print(type(recompressed))
-print(compressed == recompressed)
+print(f'Round-trip works: {decompressed == serialized}')
