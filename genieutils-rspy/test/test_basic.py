625d512f6a2da141fd4d6b46a3b3c7546bda044e
Calle Svensson <calle.svensson@zeta-two.com>
2024-12-22 02:00:34 +0100

    Add basic test

--- genieutils-rspy/test/test_basic.py
+++ genieutils-rspy/test/test_basic.py
@@ -1,42 +1,16 @@
 #!/usr/bin/env python3
 
-import genieutils_rspy
-import sys
+from genieutils_rspy import DatFile
+import time
 
 with open('test/empires2_x2_p1.dat', 'rb') as fin:
     compressed = fin.read()
 
-try:
-    decompressed = genieutils_rspy.DatFile.decompress(compressed)
-except Exception as e:
-    print(e.with_traceback())
-    sys.exit(1)
-
-datfile = genieutils_rspy.DatFile.parse(decompressed)
-
-
-print(dir(datfile))
-print(datfile.version)
-print(datfile.float_ptr_terrain_tables)
-#datfile.float_ptr_terrain_tables.append(1)
-print(datfile.float_ptr_terrain_tables)
-print(datfile.sounds)
-print(type(datfile.sounds[0]))
-print(dir(datfile.sounds[0]))
-
-for player_colour in datfile.player_colours:
-    print(player_colour)
-
-serialized1 = datfile.serialize()
-print(f'Round-trip works: {decompressed == serialized1}')
-
-print(f'Loom gold cost: {datfile.techs[22].resource_costs[0].amount}')
-datfile.techs[22].resource_costs[0].amount = 69
-print(f'Loom gold cost: {datfile.techs[22].resource_costs[0].amount}')
-
-print(f'Version: {datfile.version}')
-serialized2 = datfile.serialize()
-serialized3 = datfile.serialize()
-
-print(f'Round-trip works: {decompressed == serialized2}')
-print(f'Round-trip works: {serialized2 == serialized3}')
+for _ in range(3):
+    t0 = time.time()
+    decompressed = DatFile.decompress(compressed)
+    datfile = DatFile.parse(decompressed)
+    print(time.time()-t0)
+    serialized = datfile.serialize()
+    t1 = time.time()
+    print(f'Round-trip works: {decompressed == serialized}, took: {t1-t0:.2f} seconds')
---
+++ genieutils-rspy/test/test_scratch.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import genieutils_rspy
+import sys
+
+with open('test/empires2_x2_p1.dat', 'rb') as fin:
+    compressed = fin.read()
+
+try:
+    decompressed = genieutils_rspy.DatFile.decompress(compressed)
+except Exception as e:
+    print(e.with_traceback())
+    sys.exit(1)
+
+datfile = genieutils_rspy.DatFile.parse(decompressed)
+
+
+print(dir(datfile))
+print(datfile.version)
+print(datfile.float_ptr_terrain_tables)
+#datfile.float_ptr_terrain_tables.append(1)
+print(datfile.float_ptr_terrain_tables)
+print(datfile.sounds)
+print(type(datfile.sounds[0]))
+print(dir(datfile.sounds[0]))
+
+for player_colour in datfile.player_colours:
+    print(player_colour)
+
+serialized1 = datfile.serialize()
+print(f'Round-trip works: {decompressed == serialized1}')
+
+print(f'Loom gold cost: {datfile.techs[22].resource_costs[0].amount}')
+datfile.techs[22].resource_costs[0].amount = 69
+print(f'Loom gold cost: {datfile.techs[22].resource_costs[0].amount}')
+
+print(f'Version: {datfile.version}')
+serialized2 = datfile.serialize()
+serialized3 = datfile.serialize()
+
+print(f'Round-trip works: {decompressed == serialized2}')
+print(f'Round-trip works: {serialized2 == serialized3}')
